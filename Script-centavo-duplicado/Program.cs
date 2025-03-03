14ea5de37253c3bcb6504179bce8051a0f7bc22e
Sérgio Manoel Torres <sergio.torres@interprocess.com.br>
2025-03-03 18:15:00 -0300

    desafio 1bi ou 1cent

---
+++ Script-centavo-duplicado/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Numerics;
+
+int daysYear = 365;
+decimal centValue = 0.01m;
+
+BigInteger billionFinalYear = 0;
+BigInteger oneBillion = 1000000000;
+
+BigInteger centFinalYear = new BigInteger(centValue * 100);
+
+for (var i = 0; i < daysYear; i++ )
+{
+
+    centFinalYear *= 2;
+    billionFinalYear += oneBillion;
+}
+Console.WriteLine("Centavos duplicados por dia: " + centFinalYear);
+Console.WriteLine("//////");
+Console.WriteLine("Um bilhão por dia: " + billionFinalYear);
+Console.WriteLine("Centavos duplicados por dia: " + string.Format("{0:N0}", centFinalYear));
